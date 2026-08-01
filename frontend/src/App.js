febdfc003a2d9c0a3a2847e4c0745c7a867a33c6
yasaswini <turakayasaswini@gmail.com>
2026-08-01 22:00:36 +0530

    fix frontnd

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 import TaglineSection from "./TaglineSection";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL || "http://localhost:8000",
+  baseURL: "http://3.26.221.160:8000",
 });
 
 function App() {
